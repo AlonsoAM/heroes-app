2ba7f388d74c28c248dfabdc40e0d4bd468644d6
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-22 11:06:44 -0500

    Finalizando el layout de heroes

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarMenu } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LayoutPageComponent {
-
+  public sidebarItems: SidebarMenu[] = [
+    { label: 'listado', icon: 'label', url: './list' },
+    { label: 'Añadir', icon: 'add', url: './new-hero' },
+    { label: 'Buscar', icon: 'search', url: './search' },
+  ];
 }
